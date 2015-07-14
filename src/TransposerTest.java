2136f8737fb21c62b7b92bf10ddbccba171b21c8
JordanFitzpatrick <fitzpatrickj1@email.arizona.edu>
2015-07-14 14:28:20 -0700

    Add test methods for flat notes

--- src/TransposerTest.java
+++ src/TransposerTest.java
@@ -11,4 +11,25 @@ public class TransposerTest {
 	  public void testBasicTrans() {
 	    assertEquals("G", Transposer.trans("A", 10));
 	  }
+	
+	@Test
+	 public void testForwardWrap() {
+	   assertEquals("A", Transposer.trans("F", 4));
+	 }
+	
+	@Test
+	 public void testBackwardsWrap() {
+	   assertEquals("G", Transposer.trans("B", -4));
+	 }
+	
+	@Test
+	 public void testBbFlatNote() {
+	   assertEquals("F#", Transposer.trans("Bb", -4));
+	 }
+	
+	@Test
+	 public void testRegularFlatNote() {
+	   assertEquals("E", Transposer.trans("Db", 3));
+	 }
+	
 }
